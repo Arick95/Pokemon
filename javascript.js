5403009a8f55f0880937b057615a9eba87da1aa2
Arick95 <enrico.land_95@web.de>
2024-05-29 09:58:12 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+
+
+async function init() {
+    let pokeRepsonse = await fetch(url);
+    let pokeJson = await pokeRepsonse.json();
+    await loadingPokemonsUrls(pokeJson);
+}
+async function loadingPokemonsUrls(p){
+    pokemons = p.results
+    
+    pokemons.forEach(element => {
+        loadingPokemons(element.url)
+    });
+}
+
+async function loadingPokemons(Pokeurl){
+    let pokeRepsonse = await fetch(Pokeurl);
+    let pokeJson = await pokeRepsonse.json();
+    console.log(pokeJson)
+    document.getElementById('Pokebox').innerHTML += pokeJson.name;
+}
