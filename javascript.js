44a11f62efef3d9312bd786bdcb0c9fc21ffb516
Arick95 <enrico.land_95@web.de>
2024-05-30 18:47:19 +0200

    Load function finish

--- javascript.js
+++ javascript.js
@@ -1,26 +1,62 @@
-url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
-
+url = 'https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0';
+let poke_type_color = ['fire', 'water', 'grass', 'ground', 'rock', 'steel', 'ice', 'fairy', 'dark', 'normal', 'dragon', 'bug', 'fighting', 'psychic', 'poison', 'electric', 'ghost', 'flying'];
+let PokemonUrl;
+let type;
+let name;
+let sprite;
+let id;
 
 async function init() {
-    let pokeRepsonse = await fetch(url);
-    let pokeJson = await pokeRepsonse.json();
-    await loadingPokemonsUrls(pokeJson);
+    await urlunpacker(url)
+    await loadingPokemonsUrls(PokemonUrl);
 }
+
 async function loadingPokemonsUrls(p) {
     pokemons = p.results
-
     pokemons.forEach(element => {
         loadingPokemons(element.url)
     });
 }
 
 async function loadingPokemons(Pokeurl) {
-    let pokeRepsonse = await fetch(Pokeurl);
-    let pokeJson = await pokeRepsonse.json();
-    loadPokemonsBox(pokeJson)
+    await urlunpacker(Pokeurl);
+    loadPokemonsData();
 }
 
-function loadPokemonsBox(pokeJson) {
-    console.log(pokeJson)
-    document.getElementById('Pokebox').innerHTML += `<div id=${pokeJson.id}>hallo</div>`;
+async function urlunpacker(Pokeurl) {
+    let pokeRepsonse = await fetch(Pokeurl);
+    let pokeJson = await pokeRepsonse.json();
+    PokemonUrl = pokeJson;
 }
+
+function loadPokemonsData() {
+    name = PokemonUrl.name.charAt(0).toUpperCase()+ PokemonUrl.name.slice(1);
+    sprite = PokemonUrl.sprites.front_default;
+    id = PokemonUrl.id;
+    loadPokemons();
+    PokemonUrl.types.forEach(element => {
+        type = element.type.name;
+        type = type.charAt(0).toUpperCase()+ type.slice(1);
+        addtypes();
+    });
+}
+function addtypes() {
+    document.getElementById(`poketype${id}`).innerHTML += `<div>${type}</div>`;
+}
+
+function loadPokemons() {
+    document.getElementById('Pokecontainer').innerHTML += `  
+    <div class="Pokebox" id="Pokebox">
+        <div class="pokecheast" id="pokecheast">
+        <div class="pokespritebox" id="pokespritebox">
+            <img class="sprite" src="${sprite}">
+        </div>
+        <div class="pokenumber" id="pokenumber">${id}</div>
+        <div class="pokename" id="pokename">${name}</div>
+        <div class="poketypebox" id="poketypebox">
+            <div class="poketype" id="poketype${id}"></div>
+            </div>
+        </div>
+    </div>`;
+}
+
