0781e036577de5420f42900484548e6951f15968
Arick95 <enrico.land_95@web.de>
2024-05-30 21:16:58 +0200

    Adding Types and Backgroundcolors

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-url = 'https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0';
-let poke_type_color = ['fire', 'water', 'grass', 'ground', 'rock', 'steel', 'ice', 'fairy', 'dark', 'normal', 'dragon', 'bug', 'fighting', 'psychic', 'poison', 'electric', 'ghost', 'flying'];
+url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
 let PokemonUrl;
 let type;
 let name;
 let sprite;
 let id;
+let colorType;
 
 async function init() {
     await urlunpacker(url)
@@ -29,29 +29,37 @@ async function urlunpacker(Pokeurl) {
     PokemonUrl = pokeJson;
 }
 
-function loadPokemonsData() {
-    name = PokemonUrl.name.charAt(0).toUpperCase()+ PokemonUrl.name.slice(1);
+async function loadPokemonsData() {
+    name = PokemonUrl.name.charAt(0).toUpperCase() + PokemonUrl.name.slice(1);
     sprite = PokemonUrl.sprites.front_default;
     id = PokemonUrl.id;
-    loadPokemons();
+    await loadPokemons();
     PokemonUrl.types.forEach(element => {
         type = element.type.name;
-        type = type.charAt(0).toUpperCase()+ type.slice(1);
+        type = type.charAt(0).toUpperCase() + type.slice(1);
         addtypes();
     });
+    addBackgroundColor();
 }
 function addtypes() {
     document.getElementById(`poketype${id}`).innerHTML += `<div>${type}</div>`;
 }
 
-function loadPokemons() {
+function addBackgroundColor() {
+    PokemonUrl.types.reverse().forEach(element => {
+        colorType = element.type.name;
+    });
+    document.getElementById(`pokecheast${id}`).classList.add(`color-${colorType}`)
+}
+
+async function loadPokemons() {
     document.getElementById('Pokecontainer').innerHTML += `  
     <div class="Pokebox" id="Pokebox">
-        <div class="pokecheast" id="pokecheast">
+        <div class="pokecheast" id="pokecheast${id}">
         <div class="pokespritebox" id="pokespritebox">
             <img class="sprite" src="${sprite}">
         </div>
-        <div class="pokenumber" id="pokenumber">${id}</div>
+        <div class="pokenumber" id="pokenumber">${id}.</div>
         <div class="pokename" id="pokename">${name}</div>
         <div class="poketypebox" id="poketypebox">
             <div class="poketype" id="poketype${id}"></div>
